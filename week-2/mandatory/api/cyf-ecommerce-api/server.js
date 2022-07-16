da472c98d3d31adce3317dbef417041ae8d33310
REALcuba <angelskcuba@gmail.com>
2022-07-16 16:01:45 +0200

    DB connected and get endpoint

--- week-2/mandatory/api/cyf-ecommerce-api/server.js
+++ week-2/mandatory/api/cyf-ecommerce-api/server.js
@@ -1,25 +1,33 @@
-const express = require('express');
-const { Pool } = require('pg');
+const express = require("express");
+const { Pool } = require("pg");
 const app = express();
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello Express')
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "",
+  port: 5432,
+});
+app.get("/", (req, res) => {
+  res.send("Hello Express");
 });
 
-
 app.get("/customers", (req, res) => {
-    res.send("customers")
-}
-)
+  pool.query("SELECT * FROM customers", (error, result) => {
+    res.json(result.rows);
+  });
+});
 app.get("/suppliers", (req, res) => {
-    res.send("supliers")
-}
-)
+  pool.query("SELECT * FROM suppliers", (error, result) => {
+    res.json(result.rows);
+  });
+});
 app.get("/products", (req, res) => {
-    res.send("products")
-}
-)
-app.listen(3000, () => console.log("Server is up and running"))
+  pool.query("SELECT * FROM products", (error, result) => {
+    res.json(result.rows);
+  });
+});
+app.listen(3000, () =>
+  console.log("Server is up and running at http://127.0.0.1:3000")
+);
